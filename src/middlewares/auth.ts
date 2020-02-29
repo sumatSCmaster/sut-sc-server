0de8e59170a5b5e4afa623cce37885dae1248db7
Luis A. Acurero <13luismb@gmail.com>
2020-02-29 11:53:58 -0400

    Middleware de autorizacion actualizado

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,5 @@
 export const isAdmin = async (req, res, next) => {
-  if (req.user.admin) {
+  if (req.user.tipoUsuario === 2) {
     next();
   } else {
     res.status(401).json({
@@ -10,12 +10,12 @@ export const isAdmin = async (req, res, next) => {
 };
 
 export const isSuperuser = async (req, res, next) => {
-  if (req.user.superuser) {
+  if (req.user.tipoUsuario === 1) {
     next();
   } else {
     res.status(401).json({
       status: 401,
       message: "Forbidden. Can only be accesed by superuser."
-    })
+    });
   }
-}
+};
