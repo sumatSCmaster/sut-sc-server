60359d67da320895ed4118a89caa3e72d6489b0d
Luis A. Acurero <13luismb@gmail.com>
2020-03-14 12:27:43 -0400

    Validaciones para caso social incluidas

--- src/routes/affairs.ts
+++ src/routes/affairs.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { validate, isOfficial, isExternalUser, isLogged, isAuth } from '@validations/auth';
+import { isOfficialAdmin, createSocialCase } from '@validations/auth';
 import { checkResult } from '@validations/index';
 import { authenticate } from 'passport';
 import { fulfill } from '@utils/resolver';
@@ -8,14 +8,14 @@ import { affairInit, updateAffair } from '@helpers/affairs';
 
 const router = Router();
 
-router.post('/init', authenticate('jwt'), async (req: any, res) => {
+router.post('/init', createSocialCase, checkResult, authenticate('jwt'), isOfficialAdmin, async (req: any, res) => {
   const { caso } = req.body;
   const [error, data] = await fulfill(affairInit(caso, req.user));
   if (error) res.status(500).json(error);
   if (data) res.status(data.status).json(data);
 });
 
-router.put('/update', authenticate('jwt'), async (req: any, res) => {
+router.put('/update', authenticate('jwt'), isOfficialAdmin, async (req: any, res) => {
   const { caso } = req.body;
   const [error, data] = await fulfill(updateAffair(caso));
   if (error) res.status(500).json(error);
