e7f5aa1af12712a541f715104981cbb52bd16689
Andres Marmol <andresmarmolm@gmail.com>
2020-03-19 16:39:33 -0400

    Mkdir fix

--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -11,7 +11,7 @@ export const diskStorage = (type: string): multer.StorageEngine =>
     development: multer.diskStorage({
       destination: (req, file, cb) => {
         const saveTo = path.join(process.env.STORAGE_DIR || '/', type);
-        if (!fs.existsSync(saveTo)) fs.mkdirSync(saveTo);
+        if (!fs.existsSync(saveTo)) fs.mkdirSync(saveTo, { recursive: true });
         cb(null, saveTo);
       },
       filename: (req: any, file, cb) => {
