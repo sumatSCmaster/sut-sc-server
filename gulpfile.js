9df04500250fdc4caf1ea8161f4257e339a8cf14
Gabriel Trompiz <gab.tc@icloud.com>
2020-03-21 19:24:46 -0400

    fix en pipe de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const destBuildFolder = 'build';
 
 function defaultTask(cb) {
     let result = tsProject.src()
+    .pipe(tsProject())
     result.pipe(gulp.dest(`${destBuildFolder}`));
     gulp.src('./src/views/planillas/*.pug').pipe(gulp.dest(`./${destBuildFolder}/views/planillas`));
     gulp.src('./src/views/planillas/css/*.css').pipe(gulp.dest(`./${destBuildFolder}/views/planillas/css`));
