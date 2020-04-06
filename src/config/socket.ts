058905f54cfb7b3bffb2e54c1b7a3f70d427031b
Luis A. Acurero <13luismb@gmail.com>
2020-04-06 12:23:47 -0400

    Configuracion inicial de notificaciones

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,5 +1,9 @@
 import { Socket } from 'socket.io';
 import { decode } from 'jwt-simple';
+import Pool from '@utils/Pool';
+import queries from '@utils/queries';
+
+const pool = Pool.getInstance();
 
 let io: Socket;
 const users = new Map<string, Socket>();
@@ -8,9 +12,21 @@ const connection = (socket: Socket) => {
   try {
     const user = decode(socket.handshake.query.token, process.env.JWT_SECRET || 'not a secret').sub;
     users.set(user.cedula, socket);
+    if (user.tipoUsuario === 3 && user.recaudos && user.recaudos.length > 0) user.recaudos.map(el => socket.join(`tram:${el}`));
+    else if (user.institucion && user.tipoUsuario !== 3) socket.join(`inst:${user.institucion.id}`);
+
+    if (user.tipoUsuario === 1) {
+      pool.connect().then(r => {
+        r.query(queries.GET_ALL_INSTITUTION).then(institucion => {
+          institucion.rows.map(el => socket.join(`inst:${el.id}`));
+        });
+      });
+    }
+
     console.log(`User connected: ${user.cedula}`);
     socket.on('disconnect', () => {
       users.delete(user.cedula);
+      socket.leaveAll();
     });
   } catch (e) {
     throw e;
