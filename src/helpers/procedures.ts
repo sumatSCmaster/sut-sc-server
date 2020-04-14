b020e216fd601d91277321174b82122e8da8094b
Luis A. Acurero <13luismb@gmail.com>
2020-04-14 09:41:27 -0400

    Mas fixes en instancias de tramite

--- src/helpers/procedures.ts
+++ src/helpers/procedures.ts
@@ -74,10 +74,9 @@ export const getAvailableProceduresOfInstitution = async (req: {
 
 const getProcedureInstances = async (user, client: PoolClient) => {
   try {
-    let response = //TODO: corregir el handler para que no sea tan forzado
-    (
+    let response = ( //TODO: corregir el handler para que no sea tan forzado
       await procedureInstanceHandler(
-        user.institucion && user.institucion.id === 0 ? 0 : user.institucion.nombreCorto === 'SEDETEMA' ? 6 : user.tipoUsuario,
+        user.institucion && user.institucion.id === 0 ? 0 : user.institucion && user.institucion.nombreCorto === 'SEDETEMA' ? 6 : user.tipoUsuario,
         user.tipoUsuario !== 4 ? (user.institucion ? user.institucion.id : 0) : user.id,
         client
       )
