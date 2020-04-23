d28617d36329976f5924093006f34deeb1091d04
Luis A. Acurero <13luismb@gmail.com>
2020-04-23 11:35:45 -0400

    Fix en propiedad hasNotification

--- src/utils/Strategies.ts
+++ src/utils/Strategies.ts
@@ -5,7 +5,7 @@ import { Strategy as Local, VerifyFunction } from 'passport-local';
 import { getUserByUsername, comparePassword, verifyExternalUser, initialExtUserSignUp, getByOAuthID } from '@helpers/user';
 import { encode } from 'jwt-simple';
 import { Usuario, TipoUsuario } from '@interfaces/sigt';
-import { hasNotifications } from '@helpers/user'
+import { hasNotifications } from '@helpers/user';
 
 const optJwt = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -37,8 +37,8 @@ const GoogleStrategy = new Google(optGoogle, async (accessToken, refreshToken, p
   }
   if (request?.data.length > 0) {
     const exists = await verifyExternalUser(request?.data[0].id_usuario);
-    if(exists){
-      exists.hasNewNotifications = await hasNotifications(exists.cedula);
+    if (exists) {
+      exists.hasNewNotifications = await hasNotifications(`${exists.nacionalidad}-${exists.cedula}`);
     }
     return exists
       ? done(null, {
@@ -56,7 +56,7 @@ const GoogleStrategy = new Google(optGoogle, async (accessToken, refreshToken, p
   };
 
   request = await initialExtUserSignUp(googleOpts);
-  request.hasNewNotifications = await hasNotifications(request.cedula);
+  request.hasNewNotifications = await hasNotifications(`${request.nacionalidad}-${request.cedula}`);
   if (request) {
     return done(null, { ...request, nombreUsuario: googleOpts.email });
   } else {
@@ -71,8 +71,8 @@ const FacebookStrategy = new Facebook(optFacebook, async (accessToken, refreshTo
   }
   if (request?.data.length > 0) {
     const exists = await verifyExternalUser(request?.data[0].id_usuario);
-    if(exists){
-      exists.hasNewNotifications = await hasNotifications(exists.cedula);
+    if (exists) {
+      exists.hasNewNotifications = await hasNotifications(`${exists.nacionalidad}-${exists.cedula}`);
     }
     return exists ? done(null, exists) : done(null);
   }
@@ -84,7 +84,7 @@ const FacebookStrategy = new Facebook(optFacebook, async (accessToken, refreshTo
   };
 
   request = await initialExtUserSignUp(facebookOpts);
-  request.hasNewNotifications = await hasNotifications(request.cedula);
+  request.hasNewNotifications = await hasNotifications(`${request.nacionalidad}-${request.cedula}`);
   if (request) {
     return done(null, request);
   } else {
@@ -108,11 +108,11 @@ const verifyLocal: VerifyFunction = async (username: string, password: string, d
       });
     }
 
-    const newNotifications = await hasNotifications(user.cedula);
+    const newNotifications = await hasNotifications(`${user.nacionalidad}-${user.cedula}`);
     return done(null, {
       ...user,
       password: undefined,
-      hasNewNotifications: newNotifications
+      hasNewNotifications: newNotifications,
     });
   } else {
     return done(null, false, { message: 'Usuario/Contraseña invalida' });
