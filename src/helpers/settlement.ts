b80dba3ce3c1d8e28630f0cb1f36074241a4a621
Andres Marmol <andresmarmolm@gmail.com>
2020-05-22 11:58:36 -0400

    Fix

--- src/helpers/settlement.ts
+++ src/helpers/settlement.ts
@@ -358,25 +358,62 @@ const mesesCardinal = {
 };
 const createSolvencyForApplication = async ({ gticPool, pool, user, application }: CertificatePayload) => {
   try {
-  const isJuridical = application.tipoContribuyente === 'JURIDICO';
-  const queryContribuyente = isJuridical? queries.gtic.JURIDICAL_CONTRIBUTOR_EXISTS : queries.gtic.NATURAL_CONTRIBUTOR_EXISTS;
-  const payloadContribuyente = isJuridical ? [application.documento, application.rim, application.nacionalidad] : [application.nacionalidad, application.nacionalidad]
-  const datosContribuyente = (await gticPool.query(queryContribuyente, payloadContribuyente)).rows[0]
-  const linkQr = await qr.toDataURL(`${process.env.CLIENT_URL}/sedemat/${application.idLiquidacion}`, { errorCorrectionLevel: 'H' });
-  return new Promise(async (res, rej) => {
-    const html = renderFile(resolve(__dirname, `../views/planillas/sedemat-solvencia-AE.pug`), {
-      moment: require('moment'),
-      datos: {
-        contribuyente: isJuridical ? datosContribuyente.tx_razon_social : datosContribuyente.nb_contribuyente + datosContribuyente.ap_contribuyente,
-        rim: application.rim,
-        cedulaORif: application.nacionalidad + '-' + application.documento,
-        direccion: datosContribuyente.tx_direccion,
-        representanteLegal: datosContribuyente.nb_representante_legal,
-        periodo: mesesCardinal[application.mes],
-        anio: application.anio,
-        fecha: moment(),
-        fechaLetra: `${moment().date()} de ${application.mes} de ${application.anio}`,
-        QR: linkQr
+    const isJuridical = application.tipoContribuyente === 'JURIDICO';
+    const queryContribuyente = isJuridical ? queries.gtic.JURIDICAL_CONTRIBUTOR_EXISTS : queries.gtic.NATURAL_CONTRIBUTOR_EXISTS;
+    const payloadContribuyente = isJuridical
+      ? [application.documento, application.rim, application.nacionalidad]
+      : [application.nacionalidad, application.nacionalidad];
+    const datosContribuyente = (await gticPool.query(queryContribuyente, payloadContribuyente)).rows[0];
+    const linkQr = await qr.toDataURL(`${process.env.CLIENT_URL}/sedemat/${application.id}`, { errorCorrectionLevel: 'H' });
+    return new Promise(async (res, rej) => {
+      const html = renderFile(resolve(__dirname, `../views/planillas/sedemat-solvencia-AE.pug`), {
+        moment: require('moment'),
+        datos: {
+          contribuyente: isJuridical ? datosContribuyente.tx_razon_social : datosContribuyente.nb_contribuyente + datosContribuyente.ap_contribuyente,
+          rim: application.rim,
+          cedulaORif: application.nacionalidad + '-' + application.documento,
+          direccion: datosContribuyente.tx_direccion,
+          representanteLegal: datosContribuyente.nb_representante_legal,
+          periodo: mesesCardinal[application.mes],
+          anio: application.anio,
+          fecha: moment(),
+          fechaLetra: `${moment().date()} de ${application.mes} de ${application.anio}`,
+          QR: linkQr,
+        },
+      });
+      const pdfDir = resolve(__dirname, `../../archivos/sedemat/${application.id}/AE/${application.idLiquidacion}/solvencia.pdf`);
+      const dir = `${process.env.SERVER_URL}/archivos/sedemat/${application.id}/AE/${application.idLiquidacion}/solvencia.pdf`;
+      if (dev) {
+        pdf
+          .create(html, { format: 'Letter', border: '5mm', header: { height: '0px' }, base: 'file://' + resolve(__dirname, '../views/planillas/') + '/' })
+          .toFile(pdfDir, () => {
+            res(dir);
+          });
+      } else {
+        // try {
+        //   pdf
+        //     .create(html, { format: 'Letter', border: '5mm', header: { height: '0px' }, base: 'file://' + resolve(__dirname, '../views/planillas/') + '/' })
+        //     .toBuffer(async (err, buffer) => {
+        //       if (err) {
+        //         rej(err);
+        //       } else {
+        //         const bucketParams = {
+        //           Bucket: 'sut-maracaibo',
+        //           Key: estado === 'iniciado' ? `${institucion}/planillas/${codigo}` : `${institucion}/certificados/${codigo}`,
+        //         };
+        //         await S3Client.putObject({
+        //           ...bucketParams,
+        //           Body: buffer,
+        //           ACL: 'public-read',
+        //           ContentType: 'application/pdf',
+        //         }).promise();
+        //         res(`${process.env.AWS_ACCESS_URL}/${bucketParams.Key}`);
+        //       }
+        //     });
+        // } catch (e) {
+        //   throw e;
+        // } finally {
+        // }
       }
     });
   } catch (error) {
