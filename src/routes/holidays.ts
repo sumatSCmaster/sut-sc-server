85e7a66a1c9ff26dedacad93d1f7be8ecbc0b160
Andres Marmol <andresmarmolm@gmail.com>
2020-05-27 11:34:29 -0400

    Mensajes en rutas de dias feriados

--- src/routes/holidays.ts
+++ src/routes/holidays.ts
@@ -9,19 +9,19 @@ const router = Router();
 router.get('/', authenticate('jwt'), isOfficialAdmin ,async (req,res) => {
     const [error, data] = await fulfill(getHolidays());
     if (error) res.status(500).json({ error, status: 500 });
-    if (data) res.status(200).json({ holidays: data });
+    if (data) res.status(200).json({ holidays: data, status: 200, message: 'Dias feriados obtenidos' });
 });
 
 router.post('/', authenticate('jwt'), isOfficialAdmin ,async (req,res) => {
     const [error, data] = await fulfill(createHolidays(req.body.data));
     if (error) res.status(500).json({ error, status: 500 });
-    if (data) res.status(200).json({ holidays: data });
+    if (data) res.status(200).json({ holidays: data, status: 200, message: 'Dias feriados creados'});
 });
 
 router.delete('/:id', authenticate('jwt'), isOfficialAdmin ,async (req,res) => {
     const [error, data] = await fulfill(deleteHoliday(req.params['id']));
     if (error) res.status(500).json({ error, status: 500 });
-    if (data) res.status(200).json({ holiday: data });
+    if (data) res.status(200).json({ holiday: data, status: 200, message: 'Dia feriado eliminado' });
 });
 
 
