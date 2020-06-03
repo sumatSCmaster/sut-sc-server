870716b5e797ff23e1f44efac4b8c8ba6963655e
Andres Marmol <andresmarmolm@gmail.com>
2020-06-03 16:33:28 -0400

    Check de notificaciones y fix en query para PP

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -575,6 +575,9 @@ WHERE ttr.id_tipo_tramite=$1 AND ttr.fisico = false ORDER BY rec.id_recaudo',
       IU: {
         table: 'iu_desglose',
       },
+      PP: {
+        table: 'pp_desglose'
+      }
     };
     return `SELECT * FROM impuesto.${type[typePick].table} d INNER JOIN impuesto.liquidacion l ON d.id_liquidacion = l.id_liquidacion INNER JOIN impuesto.solicitud s ON s.id_solicitud = l.id_solicitud WHERE l.id_solicitud = $1;`;
   },
