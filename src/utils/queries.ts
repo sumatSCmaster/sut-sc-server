25e685eee6fea1219f12833e2e2a921b87baf141
Luis A. Acurero <13luismb@gmail.com>
2020-06-04 12:19:33 -0400

    mas fixes en estadisticas

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -479,7 +479,8 @@ WHERE ttr.id_tipo_tramite=$1 AND ttr.fisico = false ORDER BY rec.id_recaudo',
     'SELECT COUNT (*) FROM impuesto.solicitud sl INNER JOIN impuesto.liquidacion li ON sl.id_solicitud = li.id_solicitud WHERE sl.aprobado = false',
   GET_COMPLETED_APPLICATION_TOTAL: 'SELECT COUNT (*) FROM impuesto.solicitud WHERE aprobado = true',
   GET_MONTHLY_COMPLETED_APPLICATION_TOTAL: 'SELECT COUNT (*) FROM impuesto.solicitud WHERE EXTRACT(MONTH FROM fecha) = $1 AND aprobado = true',
-  GET_RAISED_MONEY_BY_BRANCH: 'SELECT SUM("montoLiquidacion"), "tipoLiquidacion" FROM impuesto.solicitud_view GROUP BY "tipoLiquidacion"',
+  GET_RAISED_MONEY_BY_BRANCH:
+    'SELECT SUM("montoLiquidacion"), "tipoLiquidacion" FROM impuesto.solicitud_view WHERE "fechaCreacion"::date = CURRENT_DATE::date GROUP BY "tipoLiquidacion" ORDER BY sum DESC',
   GET_SETTLEMENTS_BY_DAY:
     "SELECT COUNT (*), sl.fecha::date AS fecha_creacion FROM impuesto.solicitud sl INNER JOIN impuesto.liquidacion li ON sl.id_solicitud = li.id_solicitud AND sl.fecha::date > CURRENT_DATE - INTERVAL '30 days' GROUP BY sl.fecha::date;",
   GET_APPLICATION_COUNT_LAST_20_DAYS:
