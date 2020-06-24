8780d1aadeb0c666049a3a47c76902062cb247aa
Andres Marmol <andresmarmolm@gmail.com>
2020-06-24 17:11:34 -0400

    Post exoneracion para contribuyentes

--- src/helpers/exonerations.ts
+++ src/helpers/exonerations.ts
@@ -84,8 +84,41 @@ export const getBranchExonerations = async () => {
     }
 }
 
-export const createContributorExoneration = async () => {
+export const createContributorExoneration = async ({idContributor, from, activities}: { idContributor: number, from: Date, activities: any[]  }) => {
+    const client = await pool.connect()
+    try{
+        await client.query('BEGIN');
+        const exoneration = (await client.query(queries.CREATE_EXONERATION, [from])).rows[0];
+        if(activities){
+            await Promise.all(activities.map(async (row) => {
+                if((await client.query(queries.GET_EXONERATED_ACTIVITY_BY_CONTRIBUTOR, [idContributor, row.idActividadEconomica])).rowCount > 0){
+                    throw new Error(`La actividad ${row.descripcion} ya esta exonerada para este contribuyente`)
+                }else if(!((await client.query(queries.GET_CONTRIBUTOR_HAS_ACTIVITY, [idContributor, row.idActividadEconomica])).rowCount > 0)) {
+                    throw new Error(`El contribuyente no tiene esa actividad economica.`)
+                } else {
+                    return client.query(queries.INSERT_CONTRIBUTOR_EXONERATED_ACTIVITY, [exoneration.id_plazo_exoneracion, idContributor, row.idActividadEconomica]);
+                }
+            }))
+        }else {
+            if((await client.query(queries.GET_EXONERATED_CONTRIBUTOR_STATUS, [idContributor])).rowCount > 0){
+                throw new Error('El contribuyente ya está exonerado')
+            }else {
+                await client.query(queries.INSERT_EXONERATION_CONTRIBUTOR, [exoneration.id_plazo_exoneracion ,idContributor])
+            }
+        }
+        
+        await client.query('COMMIT');
+        return {
+            message: 'Exoneracion creada'   
+        }
 
+    } catch (e) {
+        await client.query('ROLLBACK');
+        console.error(e)
+        throw e;
+    } finally {
+        client.release()
+    }
 }
 
 export const createActivityExoneration = async () => {
@@ -93,5 +126,5 @@ export const createActivityExoneration = async () => {
 }
 
 export const createBranchExoneration = async () => {
-    
+
 }
--- src/routes/exonerations.ts
+++ src/routes/exonerations.ts
@@ -1,27 +1,32 @@
 import { Router } from 'express';
 import { fulfill } from '@utils/resolver';
-import { getContributorExonerations, getActivityExonerations, getBranchExonerations } from '@helpers/exonerations';
+import { getContributorExonerations, getActivityExonerations, getBranchExonerations, createContributorExoneration } from '@helpers/exonerations';
 import { authenticate } from 'passport';
 
 const router = Router();
 
-router.get('/contributor/:id', async (req, res) => {
+router.get('/contributor/:id', authenticate('jwt'), async (req, res) => {
   const [error, data] = await fulfill(getContributorExonerations({ contributorId: req.params['id'] }));
   if (error) res.status(500).json({ error, status: 500 });
   if (data) res.status(200).json({ status: 200, data });
 });
 
-router.get('/activities', async (req, res) => {
+router.get('/activities', authenticate('jwt'), async (req, res) => {
   const [error, data] = await fulfill(getActivityExonerations());
   if (error) res.status(500).json({ error, status: 500 });
   if (data) res.status(200).json({ status: 200, data });
 });
 
-router.get('/branches', async (req, res) => {
+router.get('/branches', authenticate('jwt'), async (req, res) => {
     const [error, data] = await fulfill(getBranchExonerations());
     if (error) res.status(500).json({ error, status: 500 });
     if (data) res.status(200).json({ status: 200, data });
   });
 
+router.post('/contributor', authenticate('jwt'), async (req, res) => {
+    const [error, data] = await fulfill(createContributorExoneration(req.body))
+    if (error) res.status(500).json({ error, status: 500 });
+    if (data) res.status(200).json({ status: 200, data });
+})
 
 export default router;
