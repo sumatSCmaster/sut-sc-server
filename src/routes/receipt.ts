84a8ce91b0cd9f353d01c5beede39bfb61536252
Andres Marmol <andresmarmolm@gmail.com>
2020-07-09 09:36:43 -0400

    Fix

--- src/routes/receipt.ts
+++ src/routes/receipt.ts
@@ -7,8 +7,7 @@ const router = Router();
 
 
 router.post('/:id', authenticate('jwt'), async (req, res) => {
-  const { id } = req.body;
-  const [error, data] = await fulfill(generateReceipt(req.user ,{application: id}));
+  const [error, data] = await fulfill(generateReceipt(req.user ,{application: +req.params['id']}));
   console.log(error, data)
   if (error) res.status(500).json({ error, status: 500 });
   if (data) res.status(200).json({ status: 200, data });
