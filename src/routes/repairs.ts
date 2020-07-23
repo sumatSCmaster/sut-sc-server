63ac623f5f026d50df2442a3b808f0913a11872a
Luis A. Acurero <13luismb@gmail.com>
2020-07-23 14:56:42 -0400

    iba a funcionar bastante sin el endpoint

--- src/routes/repairs.ts
+++ src/routes/repairs.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 
 import { fulfill } from '@utils/resolver';
-import { insertRetentions } from '@helpers/retention';
 import { authenticate } from 'passport';
+import { insertRepairs } from '@helpers/repairs';
 
 const router = Router();
 
 router.post('/init', authenticate('jwt'), async (req, res) => {
-  const [error, data] = await fulfill(insertRetentions({ process: req.body, user: req.user }));
+  const [error, data] = await fulfill(insertRepairs({ process: req.body, user: req.user }));
   if (error) res.status(500).json(error);
   if (data) res.status(data.status).json(data);
 });
