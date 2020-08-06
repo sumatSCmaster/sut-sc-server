f0d0b9ce61fcd8d20b25e8550cd9a2f24efb30f2
Andres Marmol <andresmarmolm@gmail.com>
2020-08-06 15:55:00 -0400

    Q tramite

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -729,7 +729,7 @@ l.id_subramo = sr.id_subramo INNER JOIN impuesto.ramo rm ON sr.id_ramo = rm.id_r
     FROM (SELECT * FROM pago p 
             INNER JOIN tramite t ON t.id_tramite = p.id_procedimiento 
             INNER JOIN tipo_tramite tt ON t.id_tipo_tramite = tt.id_tipo_tramite 
-            WHERE p.concepto = 'TRAMITE' AND tt.id_institucion = 9 AND p.metodo_pago = 'EFECTIVO' AND p.fecha_de_pago BETWEEN $1 AND $2) p
+            WHERE p.concepto = 'TRAMITE' AND tt.id_institucion = 9 AND p.metodo_pago = 'EFECTIVO' AND p.fecha_de_aprobacion BETWEEN $1 AND $2) p
   ) x GROUP BY moneda;`,
   GET_CREDIT_REPORT: `SELECT sum(monto) as total FROM pago WHERE metodo_pago = 'CREDITO_FISCAL' AND fecha_de_pago BETWEEN $1 AND $2`,
   GET_CREDIT_INGRESS_BY_INTERVAL: `SELECT COALESCE(SUM(credito), 0) AS ingresado, COALESCE(COUNT(*), 0) AS "cantidadIng" FROM impuesto.credito_fiscal WHERE credito > 0 AND importado != true AND fecha_creacion BETWEEN $1 AND $2;`,
