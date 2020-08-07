ddd6f69ab8a8d1d3b3e7a55823dd2d90b4c01373
Andres Marmol <andresmarmolm@gmail.com>
2020-08-07 14:04:41 -0400

    rebate

--- src/helpers/settlement.ts
+++ src/helpers/settlement.ts
@@ -2820,10 +2820,7 @@ export const internalUserLinking = async (data) => {
 
 const recursiveRebate = (array, number, abs): any[] => {
   const minus = abs ? number / array.filter((a) => +a.monto > 0).length : number / array.length;
-  let _array = array.map((e) => {
-    e.monto = +e.monto > 0 ? +e.monto - minus : +e.monto;
-    return e;
-  });
+  let _array = array.map((e) => +e.monto > 0 ? +e.monto - minus : +e.monto);
   const diff = Math.abs(_array.filter((e) => +e.monto < 0).reduce((prev, current) => prev + +current.monto, 0));
   if (diff > 0) {
     return recursiveRebate(
