6cce789a24300e490d60816d6607a838056a612f
Luis A. Acurero <13luismb@gmail.com>
2020-08-10 11:17:58 -0400

    coro yaaaa

--- src/helpers/retention.ts
+++ src/helpers/retention.ts
@@ -243,7 +243,7 @@ export const insertRetentions = async ({ process, user }) => {
             try {
               await client.query(queries.CREATE_RETENTION_DETAIL, [liquidacion.id_liquidacion, x.rif, x.rim, x.razonSocial, x.tipoServicio, x.fecha, x.baseImponible, x.montoRetenido, x.porcentaje, x.codActividad, x.numeroFactura]);
             } catch (e) {
-              throw { status: 403, message: `Verifique el rif: ${x.rif} dentro de su declaracion de ${el.fechaCancelada.month} ${el.fechaCancelada.year}, pues este posee un RIM asociado.`, rif: x.rif };
+              throw { status: 403, message: `Verifique el rif: ${x.rif} dentro de su declaracion de ${el.fechaCancelada.month} ${el.fechaCancelada.year}, pues este posee un RIM asociado.`, rif: x.rif, fechaCancelada: el.fechaCancelada };
             }
           })
         );
