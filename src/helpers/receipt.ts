450e9c69635cf804b8c3984558ef4dea55fc032d
Luis A. Acurero <13luismb@gmail.com>
2020-08-10 14:49:00 -0400

    dubara di

--- src/helpers/receipt.ts
+++ src/helpers/receipt.ts
@@ -8,7 +8,7 @@ import { renderFile } from 'pug';
 import { errorMessageExtractor } from './errors';
 import * as pdf from 'html-pdf';
 import * as qr from 'qrcode';
-import { chunk } from 'lodash'
+import { chunk } from 'lodash';
 const dev = process.env.NODE_ENV !== 'production';
 
 const pool = Pool.getInstance();
@@ -17,23 +17,22 @@ export const generateReceipt = async (payload: { application: number }) => {
   const client = await pool.connect();
   const applicationView = (await client.query(queries.GET_APPLICATION_VIEW_BY_ID, [payload.application])).rows[0];
   const payment = (await client.query(queries.GET_PAYMENT_FROM_REQ_ID_GROUP_BY_PAYMENT_TYPE, [applicationView.id])).rows;
-  const paymentRows = (await client.query(queries.GET_PAYMENT_FROM_REQ_ID, [applicationView.id, 'IMPUESTO'])).rows
-  console.log('payment', payment)
-  console.log('paymentRows' ,paymentRows)
-  const paymentTotal = payment.reduce((prev, next) => prev + (+next.monto) , 0);
-  console.log('paymentTotal', paymentTotal)
+  const paymentRows = (await client.query(queries.GET_PAYMENT_FROM_REQ_ID, [applicationView.id, 'IMPUESTO'])).rows;
+  console.log('payment', payment);
+  console.log('paymentRows', paymentRows);
+  const paymentTotal = payment.reduce((prev, next) => prev + +next.monto, 0);
+  console.log('paymentTotal', paymentTotal);
   const cashier = (await client.query(queries.GET_USER_INFO_BY_ID, [paymentRows[0].id_usuario])).rows;
   const breakdownData = (await client.query(queries.GET_SETTLEMENT_INSTANCES_BY_APPLICATION_ID, [applicationView.id])).rows;
   const referencia = (await pool.query(queries.REGISTRY_BY_SETTLEMENT_ID, [applicationView.idLiquidacion])).rows[0];
-  console.log('breakdowndata', breakdownData)
-  
-  try {
+  console.log('breakdowndata', breakdownData);
 
+  try {
     return new Promise(async (res, rej) => {
       const pdfDir = resolve(__dirname, `../../archivos/sedemat/recibo/${applicationView.id}/cierre.pdf`);
       const dir = `${process.env.SERVER_URL}/sedemat/recibo/${applicationView.id}/recibo.pdf`;
-      let total = breakdownData.reduce((prev,next) => prev + (+next.monto), 0);
-      console.log('total',total)
+      let total = breakdownData.reduce((prev, next) => prev + +next.monto, 0);
+      console.log('total', total);
       const linkQr = await qr.toDataURL(dev ? dir : `${process.env.AWS_ACCESS_URL}/sedemat/recibo/${applicationView.id}/recibo.pdf`, { errorCorrectionLevel: 'H' });
       const html = renderFile(resolve(__dirname, `../views/planillas/sedemat-recibo.pug`), {
         moment: require('moment'),
@@ -47,16 +46,19 @@ export const generateReceipt = async (payload: { application: number }) => {
           cajero: cashier?.[0]?.nombreCompleto,
           rim: referencia?.referencia_municipal,
           telefono: referencia?.telefono_celular,
-          items: chunk(breakdownData.map((row) => {
-            return {
-              descripcion: `${row.datos.descripcion ? row.datos.descripcion : `${row.descripcionRamo} - ${row.descripcionSubramo}`} (${row.datos.fecha.month} ${row.datos.fecha.year})`,
-              fecha: row.fechaLiquidacion,
-              monto: row.monto,
-            };
-          }),14),
+          items: chunk(
+            breakdownData.map((row) => {
+              return {
+                descripcion: `${row.datos.descripcion ? row.datos.descripcion : `${row.descripcionRamo} - ${row.descripcionSubramo}`} (${row.datos.fecha.month} ${row.datos.fecha.year})`,
+                fecha: row.fechaLiquidacion,
+                monto: row.monto,
+              };
+            }),
+            14
+          ),
           metodoPago: payment,
           total: total,
-          credito: paymentTotal - total
+          credito: paymentTotal - total,
         },
       });
       if (dev) {
@@ -65,13 +67,12 @@ export const generateReceipt = async (payload: { application: number }) => {
         });
       } else {
         try {
-          
           pdf.create(html, { format: 'Letter', border: '5mm', header: { height: '0px' }, base: 'file://' + resolve(__dirname, '../views/planillas/') + '/' }).toBuffer(async (err, buffer) => {
             if (err) {
               rej(err);
             } else {
               const regClient = await pool.connect();
-              try{
+              try {
                 await regClient.query('BEGIN');
                 const bucketParams = {
                   Bucket: 'sut-maracaibo',
@@ -83,14 +84,14 @@ export const generateReceipt = async (payload: { application: number }) => {
                   ACL: 'public-read',
                   ContentType: 'application/pdf',
                 }).promise();
-                await regClient.query(queries.INSERT_RECEIPT_RECORD, [paymentRows[0].id_usuario, `${process.env.AWS_ACCESS_URL}/${bucketParams.Key}`,  applicationView.razonSocial, referencia?.referencia_municipal ])
-                await regClient.query('COMMIT')
+                await regClient.query(queries.INSERT_RECEIPT_RECORD, [paymentRows[0].id_usuario, `${process.env.AWS_ACCESS_URL}/${bucketParams.Key}`, applicationView.razonSocial, referencia?.referencia_municipal]);
+                await regClient.query('COMMIT');
                 res(`${process.env.AWS_ACCESS_URL}/${bucketParams.Key}`);
               } catch (e) {
-                await regClient.query('ROLLBACK')
-                rej(e)
-              } finally{
-                regClient.release()
+                await regClient.query('ROLLBACK');
+                rej(e);
+              } finally {
+                regClient.release();
               }
             }
           });
@@ -107,23 +108,22 @@ export const generateReceipt = async (payload: { application: number }) => {
   }
 };
 
-export const generateRepairReceipt = async (payload: { application: number,  breakdownData, total}) => {
+export const generateRepairReceipt = async (payload: { application: number; breakdownData; total; cashier }) => {
   const client = await pool.connect();
   const applicationView = (await client.query(queries.GET_APPLICATION_VIEW_BY_ID, [payload.application])).rows[0];
   const payment = (await client.query(queries.GET_PAYMENT_FROM_REQ_ID_GROUP_BY_PAYMENT_TYPE, [applicationView.id])).rows;
-  const paymentRows = (await client.query(queries.GET_PAYMENT_FROM_REQ_ID, [applicationView.id, 'IMPUESTO'])).rows
-  console.log('payment', payment)
-  console.log('paymentRows' ,paymentRows)
-  const paymentTotal = payment.reduce((prev, next) => prev + (+next.monto) , 0);
-  console.log('paymentTotal', paymentTotal)
-  const cashier = (await client.query(queries.GET_USER_INFO_BY_ID, [paymentRows[0].id_usuario])).rows;
+  const paymentRows = (await client.query(queries.GET_PAYMENT_FROM_REQ_ID, [applicationView.id, 'IMPUESTO'])).rows;
+  console.log('payment', payment);
+  console.log('paymentRows', paymentRows);
+  const paymentTotal = payment.reduce((prev, next) => prev + +next.monto, 0);
+  console.log('paymentTotal', paymentTotal);
+  // const cashier = (await client.query(queries.GET_USER_INFO_BY_ID, [paymentRows[0]?.id_usuario])).rows;
   // const breakdownData = (await client.query(queries.GET_SETTLEMENT_INSTANCES_BY_APPLICATION_ID, [applicationView.id])).rows;
   const referencia = (await pool.query(queries.REGISTRY_BY_SETTLEMENT_ID, [applicationView.idLiquidacion])).rows[0];
   // console.log('breakdowndata', breakdownData)
-  const aforos: any[] = []
-  payload.breakdownData.map(el=> el.desglose.map(x => aforos.push({...x, fecha: el.fecha})));
+  const aforos: any[] = [];
+  payload.breakdownData.map((el) => el.desglose.map((x) => aforos.push({ ...x, fecha: el.fecha })));
   try {
-
     return new Promise(async (res, rej) => {
       const pdfDir = resolve(__dirname, `../../archivos/sedemat/recibo/${applicationView.id}/cierre.pdf`);
       const dir = `${process.env.SERVER_URL}/sedemat/recibo/${applicationView.id}/recibo.pdf`;
@@ -139,18 +139,21 @@ export const generateRepairReceipt = async (payload: { application: number,  bre
           tipoDocumento: applicationView.tipoDocumento,
           documentoIden: applicationView.documento,
           direccion: applicationView.direccion,
-          cajero: cashier?.[0]?.nombreCompleto,
+          cajero: payload.cashier,
           rim: referencia?.referencia_municipal,
           telefono: referencia?.telefono_celular,
-          items: chunk(aforos.map((row) => {
-            return {
-              id: row.aforo,
-              descripcion: row.descripcion,
-              mes: row.fecha.month,
-              anio: row.fecha.year,
-              fechaLiquidacion: moment().format('MM-DD-YYYY')
-            };
-          }),14),
+          items: chunk(
+            aforos.map((row) => {
+              return {
+                id: row.aforo,
+                descripcion: row.descripcion,
+                mes: row.fecha.month,
+                anio: row.fecha.year,
+                fechaLiquidacion: moment().format('MM-DD-YYYY'),
+              };
+            }),
+            14
+          ),
           metodoPago: payment,
           total: payload.total * 1.3,
           subTotal: payload.total,
@@ -163,13 +166,12 @@ export const generateRepairReceipt = async (payload: { application: number,  bre
         });
       } else {
         try {
-          
           pdf.create(html, { format: 'Letter', border: '5mm', header: { height: '0px' }, base: 'file://' + resolve(__dirname, '../views/planillas/') + '/' }).toBuffer(async (err, buffer) => {
             if (err) {
               rej(err);
             } else {
               const regClient = await pool.connect();
-              try{
+              try {
                 await regClient.query('BEGIN');
                 const bucketParams = {
                   Bucket: 'sut-maracaibo',
@@ -181,14 +183,14 @@ export const generateRepairReceipt = async (payload: { application: number,  bre
                   ACL: 'public-read',
                   ContentType: 'application/pdf',
                 }).promise();
-                await regClient.query(queries.INSERT_RECEIPT_RECORD, [paymentRows[0].id_usuario, `${process.env.AWS_ACCESS_URL}/${bucketParams.Key}`,  applicationView.razonSocial, referencia?.referencia_municipal ])
-                await regClient.query('COMMIT')
+                // await regClient.query(queries.INSERT_RECEIPT_RECORD, [paymentRows[0].id_usuario, `${process.env.AWS_ACCESS_URL}/${bucketParams.Key}`, applicationView.razonSocial, referencia?.referencia_municipal]);
+                await regClient.query('COMMIT');
                 res(`${process.env.AWS_ACCESS_URL}/${bucketParams.Key}`);
               } catch (e) {
-                await regClient.query('ROLLBACK')
-                rej(e)
-              } finally{
-                regClient.release()
+                await regClient.query('ROLLBACK');
+                rej(e);
+              } finally {
+                regClient.release();
               }
             }
           });
