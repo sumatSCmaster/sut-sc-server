79fac99c7b11ebdf38388b223017ccc3ceebdc79
Luis A. Acurero <13luismb@gmail.com>
2020-08-18 15:51:43 -0400

    90% de la logica de licores lista, fix en planilla de liqs especiales

--- src/helpers/liquors.ts
+++ src/helpers/liquors.ts
@@ -28,44 +28,41 @@ const template = async (props) => {
   }
 };
 
-export const installLiquorLicense = async (data) => {
-  const client = await pool.connect();
+export const installLiquorLicense = async (data, client: PoolClient) => {
   try {
-    await client.query('BEGIN');
     const { usuario: user, costo } = (await client.query(queries.GET_PROCEDURE_DATA, [data.idTramite])).rows[0];
-    await createSettlementForProcedure({ monto: +costo, referenciaMunicipal: data.id_registro_municipal, ramo: 'LIC' }, client);
-    await client.query('COMMIT');
+    const { contribuyente, tipoLicencia } = data.funcionario;
+    const license = (
+      await client.query('INSERT INTO impuesto.licencia_licores (id_registro_municipal, tipo_licencia, fecha_inicio, fecha_fin) VALUES ($1, $2, $3, $4) RETURNING *', [
+        contribuyente.idRim,
+        tipoLicencia,
+        moment().format('MM-DD-YYYY'),
+        moment().add(2, 'years').format('MM-DD-YYYY'),
+      ])
+    ).rows[0];
+    // await createSettlementForProcedure({ monto: +costo, referenciaMunicipal: data.id_registro_municipal, ramo: 'LIC' }, client);
     return;
   } catch (error) {
-    client.query('ROLLBACK');
     console.log(error);
     throw {
       status: 500,
       error: errorMessageExtractor(error),
       message: errorMessageGenerator(error) || error.message || 'Error al crear la licencia de licores',
     };
-  } finally {
-    client.release();
   }
 };
 
-export const renewLiquorLicense = async (data) => {
-  const client = await pool.connect();
+export const renewLiquorLicense = async (data, client: PoolClient) => {
   try {
-    await client.query('BEGIN');
     const { usuario: user, costo } = (await client.query(queries.GET_PROCEDURE_DATA, [data.idTramite])).rows[0];
     await createSettlementForProcedure({ monto: +costo, referenciaMunicipal: data.id_registro_municipal, ramo: 'LIC' }, client);
-    await client.query('COMMIT');
     return;
   } catch (error) {
-    client.query('ROLLBACK');
     console.log(error);
     throw {
       status: 500,
       error: errorMessageExtractor(error),
       message: errorMessageGenerator(error) || error.message || 'Error al renovar la licencia de licores',
     };
-  } finally {
-    client.release();
   }
 };
