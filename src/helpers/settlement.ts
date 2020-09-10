6b3c0eb8ed954d441cb64f7a5949dcb64e74ed57
Luis A. Acurero <13luismb@gmail.com>
2020-09-10 11:57:03 -0400

    otro fis

--- src/helpers/settlement.ts
+++ src/helpers/settlement.ts
@@ -2507,7 +2507,7 @@ export const insertSettlements = async ({ process, user }) => {
             desglose: el.desglose ? el.desglose.map((al) => breakdownCaseHandler(el.ramo, al)) : undefined,
             fecha: { month: el.fechaCancelada.month, year: el.fechaCancelada.year },
             IVA: el.ramo === branchNames['SM'] ? (impuestos.esAgenteRetencion || impuestos.esAgenteSENIAT ? 4 : 16) : undefined,
-            esAgenteSENIAT: el.ramo === branchNames['SM'] ? impuestos.esAgenteRetencion || undefined : undefined,
+            esAgenteSENIAT: el.ramo === branchNames['SM'] ? impuestos.esAgenteSENIAT || undefined : undefined,
             esAgenteRetencion: el.ramo === branchNames['SM'] ? impuestos.esAgenteRetencion || undefined : undefined,
           };
           const liquidacion = (
