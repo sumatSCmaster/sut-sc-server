a4ecf8e7de90c51caaaa1d21fa8eda631312f460
Jose Sanchez <jsanchez.waku@gmail.com>
2020-10-01 14:33:25 -0400

    fix convenios

--- src/helpers/settlement.ts
+++ src/helpers/settlement.ts
@@ -3506,7 +3506,7 @@ export const approveContributorBenefits = async ({ data, client }: { data: any; 
             await client.query('UPDATE impuesto.solicitud SET tipo_solicitud = $1 WHERE id_solicitud = $2', ['CONVENIO', applicationAG.id_solicitud]);
             const agreement = (await client.query(queries.CREATE_AGREEMENT, [applicationAG.id_solicitud, x.porciones.length])).rows[0];
             const settlementsAG = (await client.query(queries.CHANGE_SETTLEMENT_TO_NEW_APPLICATION, [applicationAG.id_solicitud, contributorWithBranch.id_registro_municipal, x.idRamo])).rows[0];
-            const costo = (await client.query(queries.CHANGE_SETTLEMENT_BRANCH_TO_AGREEMENT, [x.idRamo, applicationAG.id_solicitud])).rows.reduce((x, j) => fixatedAmount(x + j.monto), 0);
+            const costo = (await client.query(queries.CHANGE_SETTLEMENT_BRANCH_TO_AGREEMENT, [x.idRamo, applicationAG.id_solicitud])).rows.reduce((x, j) => fixatedAmount(x + fixatedAmount(j.monto)), 0);
             console.log('//if -> costo', costo);
             const totalSolicitud = x.porciones.reduce((x, j) => fixatedAmount(x + j.monto), 0);
             console.log('//if -> totalSolicitud', totalSolicitud);
