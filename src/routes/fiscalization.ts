6e8f022a974411bc72f261fa5d510d10b2176f77
Andrés Mármol <andresmarmolm@gmail.com>
2020-10-08 13:50:18 -0400

    b

--- src/routes/fiscalization.ts
+++ src/routes/fiscalization.ts
@@ -5,19 +5,19 @@ import { getFiscalizations, createFiscalization, updateOneFiscalization } from '
 
 const router = Router();
 
-router.get('/', async (req, res) => {
+router.get('/', authenticate('jwt'), async (req, res) => {
   const [error, data] = await fulfill(getFiscalizations());
   if (error) res.status(500).json({ error, status: 500 });
   if (data) res.status(200).json({ status: 200, data });
 })
 
-router.post('/', async (req, res) => {
-  const [error, data] = await fulfill(createFiscalization(req.body));
+router.post('/', authenticate('jwt'), async (req, res) => {
+  const [error, data] = await fulfill(createFiscalization(req.user ,req.body));
   if (error) res.status(500).json({ message: error.message, status: 500 });
   if (data) res.status(200).json({ status: 200, data });
 });
 
-router.patch('/:id',  async (req, res) => {
+router.patch('/:id', authenticate('jwt'), async (req, res) => {
   const [error, data] = await fulfill(updateOneFiscalization(req.user , {idFiscalizacion: req.params['id'], ...req.body}));
   console.log(error, data)
   if (error) res.status(500).json({ error, status: 500 });
