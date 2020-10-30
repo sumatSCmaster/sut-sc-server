ae261de008d752c2fa78d44692cbab326845913e
Luis Acurero <13luismb@gmail.com>
2020-10-30 10:03:12 -0400

    a

--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -55,7 +55,11 @@ export const estateFilter = async (req, file, cb) => {
     }
     if (JSON.parse(req.query.nuevoInmueble)) {
       const res = await checkInm(req.params.id);
-      cb(null, !res);
+      if (!res) {
+        console.log(res);
+        cb(null, true);
+      }
+      cb(null, false);
       // if (res instanceof Error){
       //   throw res
       // }
