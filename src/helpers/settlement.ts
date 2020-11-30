bfc8b50fbc279101154995ba43614542c1f021c9
Luis Acurero <13luismb@gmail.com>
2020-11-30 15:18:06 -0400

    randar

--- src/helpers/settlement.ts
+++ src/helpers/settlement.ts
@@ -139,7 +139,8 @@ export const getIUTariffForContributor = async ({ estate, id, declaration, date 
     if (!id) return impuestoInmueble;
     const now = moment().locale('ES').subtract(1, 'M');
     const lastAEApplication = (await client.query(queries.CURRENT_SETTLEMENT_EXISTS_FOR_CODE_AND_RIM_OPTIMIZED, [codigosRamo.AE, id])).rows[0];
-    const filt = (await client.query(queries.CURRENT_SETTLEMENT_EXISTS_FOR_CODE_AND_RIM_OPTIMIZED, [codigosRamo.AE, id])).rows;
+    console.log('🚀 ~ file: settlement.ts ~ line 142 ~ getIUTariffForContributor ~ lastAEApplication', lastAEApplication);
+    const filt = (await client.query(queries.CURRENT_SETTLEMENT_EXISTS_FOR_CODE_AND_RIM_OPTIMIZED, [codigosRamo.AE, id])).rows.find((el) => el.datos.month === now.format('MMMM') && el.datos.year === now.year());
     console.log('🚀 ~ file: settlement.ts ~ line 143 ~ getIUTariffForContributor ~ filt', filt);
     // console.log('🚀 ~ file: settlement.ts ~ line 142 ~ getIUTariffForContributor ~ si', lastAEApplication);
     const AEDeclaration = Math.round(isNaN(+declaration!) ? fixatedAmount(lastAEApplication * PETRO) : +declaration!);
