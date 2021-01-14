629735572515d65219426134c6a2310c52f444e9
Andrés Mármol <andresmarmolm@gmail.com>
2021-01-14 11:03:28 -0400

    Bandeja

--- src/helpers/procedures.ts
+++ src/helpers/procedures.ts
@@ -136,7 +136,7 @@ const getProcedureInstances = async (user, client: PoolClient, support?) => {
       'PROCEDURES AAAAAAAAAAA',
       res.length,
       res.filter((row) => ![27, 39, 40].includes(row.tipoTramite)),
-      user
+      user.nombreCompleto
     );
     return esDaniel(user) ? res.filter((row) => ![27, 39, 40].includes(row.tipoTramite)) : res;
   } catch (error) {
