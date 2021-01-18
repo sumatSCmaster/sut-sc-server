b89a702c70c7aaace848877162ae583d24fd6b34
Andrés Mármol <andresmarmolm@gmail.com>
2021-01-18 14:21:50 -0400

    Owner condominium

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -2987,14 +2987,14 @@ WHERE descripcion_corta IN ('AE','SM','IU','PP') or descripcion_corta is null
       WHERE cont.tipo_documento = $1 AND cont.documento = $2;
   `,
   GET_CONDOMINIUM_OWNERS: `
-      SELECT id_condominio AS "idCondominio", cont.id_contribuyente as "idContribuyente", CONCAT(cont.tipo_documento, '-', cont.documento) AS documento, cont.razon_social AS "razonSocial", p.nombre as "parroquia"
+      SELECT id_condominio AS "idCondominio", cont.id_contribuyente as "idContribuyente", CONCAT(cont.tipo_documento, '-', cont.documento) AS documento, cont.razon_social AS "razonSocial", p.nombre as "parroquia", cont.direccion 
       FROM impuesto.contribuyente cont
       INNER JOIN parroquia p ON p.id = cont.id_parroquia
       INNER JOIN impuesto.condominio_propietario cp ON cp.id_contribuyente = cont.id_contribuyente
       WHERE cp.id_condominio = $1;
   `,
   GET_CONDOMINIUM_OWNER_BY_DOC: `
-      SELECT id_condominio AS "idCondominio", CONCAT(cont.tipo_documento, '-', cont.documento) AS documento, cont.razon_social AS "razonSocial", p.nombre as "parroquia"
+      SELECT id_condominio AS "idCondominio", CONCAT(cont.tipo_documento, '-', cont.documento) AS documento, cont.razon_social AS "razonSocial", p.nombre as "parroquia", cont.direccion
       FROM impuesto.contribuyente cont
       INNER JOIN parroquia p ON p.id = cont.id_parroquia
       INNER JOIN impuesto.condominio_propietario cp ON cond.id_contribuyente = cp.id_contribuyente
