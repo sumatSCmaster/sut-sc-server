f2b4a03546fed448596b75d3e775547a52249a4b
Andrés Mármol <andresmarmolm@gmail.com>
2021-01-20 12:32:51 -0400

    Opt listTaxPayments and add tranfReport

--- src/routes/branches.ts
+++ src/routes/branches.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { fulfill } from '@utils/resolver';
-import { generateBranchesReport, getBranches } from '@helpers/branches';
+import { generateBranchesReport, getBranches, getTransfersReport } from '@helpers/branches';
 import { authenticate } from 'passport';
 
 const router = Router();
@@ -14,6 +14,13 @@ router.get('/', authenticate('jwt'), async (req, res) => {
 router.post('/', async (req, res) => {
   const { from, to, alcaldia } = req.body;
   const [error, data] = await fulfill(generateBranchesReport(req.user, { from, to, alcaldia }));
+  if (error) res.status(500).json({ error, status: 500 });
+  if (data) res.status(200).json({ status: 200, data });
+});
+
+router.post('/reportTransf', authenticate('jwt'), async (req, res) => {
+  const { from, to, ramo } = req.body;
+  const [error, data] = await fulfill(getTransfersReport({ from, to }));
   if (error) res.status(500).json({ error, status: 500 });
   if (data) res.status(200).json({ status: 200, data });
 });
