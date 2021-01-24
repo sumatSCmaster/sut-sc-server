5b554bee205c69ac24f6378bc3ba22f2d8511985
Andrés Mármol <andresmarmolm@gmail.com>
2021-01-24 12:22:26 -0400

    tracer id

--- src/tracer.ts
+++ src/tracer.ts
@@ -1,3 +1,14 @@
 import tracer from 'dd-trace';
+import { verify } from 'jsonwebtoken';
 tracer.init(); // initialized in a different file to avoid hoisting
+tracer.use('express', {
+  hooks: {
+    request: (span, req, res) => {
+      if (req?.headers.authorization) {
+        const jwt: any = verify(req.headers.authorization.split(' ')[1], process.env.JWT_SECRET as string);
+        span?.setTag('user.id', jwt.id);
+      }
+    },
+  },
+});
 export default tracer;
