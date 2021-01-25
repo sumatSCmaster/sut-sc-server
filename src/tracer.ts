91df6a59fb453dff93c748092b694eabbd8d4e12
Andrés Mármol <andresmarmolm@gmail.com>
2021-01-25 12:00:52 -0400

    trace

--- src/tracer.ts
+++ src/tracer.ts
@@ -1,7 +1,6 @@
 import tracer from 'dd-trace';
 import { decode } from 'jsonwebtoken';
 import { mainLogger } from './utils/logger';
-import { inspect } from 'util';
 tracer.init(); // initialized in a different file to avoid hoisting
 tracer.use('express', {
   hooks: {
@@ -10,8 +9,8 @@ tracer.use('express', {
         mainLogger.info(`req headers ${req.headers.authorization}`);
         let token = req.headers.authorization.split(' ')[1];
         const payload = decode(token, { json: true });
-        mainLogger.info(`payload ${inspect(payload)}`);
-        span?.setTag('user.id', payload ? payload?.sub?.user?.id : null);
+        mainLogger.info(`payload ${payload?.sub?.id}`);
+        span?.setTag('user.id', payload ? payload?.sub?.id : null);
       }
     },
   },
