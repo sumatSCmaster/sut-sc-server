5b236b3f186a8efa6699b443d8820d9b08ff18dc
Andrés Mármol <andresmarmolm@gmail.com>
2021-01-25 12:08:31 -0400

    trace

--- src/tracer.ts
+++ src/tracer.ts
@@ -5,12 +5,14 @@ tracer.init(); // initialized in a different file to avoid hoisting
 tracer.use('express', {
   hooks: {
     request: (span, req, res) => {
-      if (req?.headers.authorization) {
-        mainLogger.info(`req headers ${req.headers.authorization}`);
-        let token = req.headers.authorization.split(' ')[1];
-        const payload = decode(token, { json: true });
-        mainLogger.info(`payload ${payload?.sub?.id}`);
-        span?.setTag('user.id', payload ? payload?.sub?.id : null);
+      try {
+        if (req?.headers.authorization) {
+          let token = req.headers.authorization.split(' ')[1];
+          const payload = decode(token, { json: true });
+          span?.setTag('user.id', payload ? payload?.sub?.id : null);
+        }
+      } catch (e) {
+        mainLogger.error(`Error setting span user id`);
       }
     },
   },
