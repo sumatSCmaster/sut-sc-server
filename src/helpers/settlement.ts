bbe2b4ede42499a2556f09d831227881b6c7aba0
Andrés Mármol <andresmarmolm@gmail.com>
2021-02-23 13:14:30 -0400

    l

--- src/helpers/settlement.ts
+++ src/helpers/settlement.ts
@@ -3173,7 +3173,7 @@ export const addTaxApplicationPayment = async ({ payment, interest, application,
     mainLogger.info('addTaxApplicationPayment -> solicitud', solicitud);
     const pagoSum = +payment.map((e) => fixatedAmount(+e.costo)).reduce((e, i) => e + i, 0);
     mainLogger.info('addTaxApplicationPayment -> pagoSum', pagoSum);
-    mainLogger.info(`addTaxApplicationPayment -> ${payment.map((pay) => `concepto ${pay?.concepto} referencia ${pay?.referencia} banco ${pay?.id_banco} metodo_pago ${pay.metodo_pago}`).join(' , ')}`)
+    mainLogger.info(`addTaxApplicationPayment -> ${payment.map((pay) => `concepto ${applicationType} referencia ${pay?.referencia} banco ${pay?.banco} metodo_pago ${pay.metodoPago}`).join(' , ')}`)
     if (pagoSum < fixatedAmount(+solicitud.monto_total)) throw { status: 401, message: `La suma de los montos es insuficiente para poder insertar el pago, con un déficit de Bs. ${fixatedAmount(+solicitud.monto_total) - pagoSum}` };
     const creditoPositivo = pagoSum - fixatedAmount(+solicitud.monto_total);
     await Promise.all(
