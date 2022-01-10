31d03dee74350cf184e94ee6ae5e464e19ac7a97
Jhonder Quintero <jhonderquintero@gmail.com>
2022-01-10 19:41:58 -0400

    observations authentication changed with jwt

--- src/routes/observation.ts
+++ src/routes/observation.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { fulfill } from '@utils/resolver';
 import { getObservations } from '@helpers/observations';
-import { apiCheckMiddleware } from './external/middleware/ApiKeyCheck';
+import { authenticate } from 'passport';
 
 const router = Router();
 
-router.get('/', apiCheckMiddleware, async (req: any, res) => {
+router.get('/', authenticate('jwt'), async (req: any, res) => {
   const { idTramite } = req.query;
   const [error, data] = await fulfill(getObservations(idTramite));
   if (error) res.status(500).json(error);
