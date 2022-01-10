4aa583bf0198f49d6b7143aa238c1da5a69917b9
Jhonder Quintero <jhonderquintero@gmail.com>
2022-01-10 18:47:59 -0400

    req.query fixed in observation.ts

--- src/routes/observation.ts
+++ src/routes/observation.ts
@@ -6,7 +6,7 @@ import { getObservations } from '@helpers/observations';
 const router = Router();
 
 router.get('/', authenticate('jwt'), async (req: any, res) => {
-  const { idTramite } = req.body;
+  const { idTramite } = req.query;
   const [error, data] = await fulfill(getObservations(idTramite));
   if (error) res.status(500).json(error);
   if (data) res.status(200).json(data);
