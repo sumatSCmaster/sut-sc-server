b2bc23155023c96eb2d8237276d2f6bae3a01141
Jhonder Quintero <jhonderquintero@gmail.com>
2022-01-10 19:36:12 -0400

    header authorization middleware added in observation endpoint

--- src/routes/observation.ts
+++ src/routes/observation.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { authenticate } from 'passport';
 import { fulfill } from '@utils/resolver';
 import { getObservations } from '@helpers/observations';
+import { apiCheckMiddleware } from './external/middleware/ApiKeyCheck';
 
 const router = Router();
 
-router.get('/', async (req: any, res) => {
+router.get('/', apiCheckMiddleware, async (req: any, res) => {
   const { idTramite } = req.query;
   const [error, data] = await fulfill(getObservations(idTramite));
   if (error) res.status(500).json(error);
