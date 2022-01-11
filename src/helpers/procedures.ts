05b9f1dcafbb37f95bb555b1ca3f832a8a397cce
rodrigolamus <rodrigolamus@gmail.com>
2022-01-11 10:05:05 -0400

    changing nombre_completo property place

--- src/helpers/procedures.ts
+++ src/helpers/procedures.ts
@@ -113,7 +113,7 @@ const getProcedureInstances = async (user, client: PoolClient, support?) => {
     let res: any[] = await Promise.all(
       response.map(async (el) => {
         let ordinances;
-        const lastEditor = (await client.query(queries.GET_LAST_EDITOR, [el.id])).rows[0].nombre_completo;
+        const lastEditor = (await client.query(queries.GET_LAST_EDITOR, [el.id])).rows[0];
         if (!el.pagoPrevio) {
           ordinances = ordinancesQ.filter((ord) => ord.idTramite === el.id);
         }
@@ -129,7 +129,7 @@ const getProcedureInstances = async (user, client: PoolClient, support?) => {
           fechaCreacion: el.fechacreacion,
           codigoTramite: el.codigotramite,
           usuario: el.usuario,
-          nombreUltimoEditor: lastEditor,
+          nombreUltimoEditor: lastEditor.nombre_completo,
           nombreLargo: el.nombrelargo,
           nombreCorto: el.nombrecorto,
           nombreTramiteLargo: el.nombretramitelargo,
