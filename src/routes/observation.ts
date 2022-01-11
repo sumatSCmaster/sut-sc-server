6edce88ca616a3f6ecbb55cf2ca3503e0ac39e9b
Jhonder Quintero <jhonderquintero@gmail.com>
2022-01-11 14:54:21 -0400

    codigoTramite validations

--- src/routes/observation.ts
+++ src/routes/observation.ts
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import { fulfill } from '@utils/resolver';
 import { getObservations } from '@helpers/observations';
 import { authenticate } from 'passport';
+import { mainLogger } from '@utils/logger';
 
 const router = Router();
 
 router.get('/', authenticate('jwt'), async (req: any, res) => {
+  console.log(req.query, "req.query");
+  mainLogger.info(req.query, "req");
   const { idTramite } = req.query;
   const [error, data] = await fulfill(getObservations(idTramite));
   if (error) res.status(500).json(error);
