854b68994da4bddaf39f409dfcb6080ad91da613
rodrigolamus <rodrigolamus@gmail.com>
2022-01-12 11:50:17 -0400

    same changes

--- src/helpers/estate.ts
+++ src/helpers/estate.ts
@@ -274,7 +274,7 @@ export const createBareEstate = async ({ codCat, direccion, idParroquia, metrosC
   try {
     await client.query('BEGIN');
     const codIsApproved = (await client.query(queries.GET_APPROVED_CPU_PROCEDURE, [codigoCpu])).rows[0];
-    if (!codIsApproved) throw new Error('El código ingresado no pertenece a un trámite aprobado');
+    if (!codIsApproved) throw new Error('El código ingresado no pertenece a un trámite aprobado de solvencia de inmuebles');
     const estate = (await client.query(queries.CREATE_BARE_ESTATE, [codCat, direccion, idParroquia, metrosConstruccion, metrosTerreno, tipoInmueble, dirDoc, codigoCpu])).rows[0];
     mainLogger.info(estate);
     await client.query(queries.ADD_MOVEMENT, [estate.id, userId, 'inmueble_registrado']);
