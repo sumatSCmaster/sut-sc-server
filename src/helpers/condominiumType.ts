db1e4eba7349b1e9f0039047bf3e05ba877de1d2
rodrigolamus <rodrigolamus@gmail.com>
2022-01-13 14:31:43 -0400

    creating condominium type endpoint

---
+++ src/helpers/condominiumType.ts
@@ -0,0 +1,18 @@
+import Pool from '@utils/Pool';
+import queries from '@utils/queries';
+import { errorMessageGenerator, errorMessageExtractor } from './errors';
+
+const pool = Pool.getInstance();
+
+export const getCondominiumType = async (id: number) => {
+  const client = await pool.connect();
+  try {
+    const response = (await client.query(queries.GET_CONDOMINIUM_TYPE_BY_ID, [id])).rows[0];
+    return { status: 200, message: 'tipo de condominio obtenido', tipoCondominio: response };
+  } catch (e: any) {
+    throw {
+      status: 500,
+      message: errorMessageGenerator(e) || errorMessageExtractor(e) || e.message,
+    };
+  }
+};
---
+++ src/routes/condominiumType.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { fulfill } from '@utils/resolver';
+import { authenticate } from 'passport';
+import { getCondominiumType } from '@helpers/condominiumType';
+
+const router = Router();
+
+router.get('/search', authenticate('jwt'), async (req: any, res) => {
+  const { id } = req.params;
+  const [err, data] = await fulfill(getCondominiumType(id));
+  if (err) res.status(500).json({ err, status: 500 });
+  if (data) res.status(200).json(...data);
+});
+
+router.put('/edit', authenticate('jwt'), async (req: any, res) => {});
+
+export default router;
