3f65f78faefa3e0a1e3f01f34fa08e9afeb8dbe3
rodrigolamus <rodrigolamus@gmail.com>
2022-01-15 12:40:07 -0400

    fix in condominiumType helper

--- src/routes/condominiumType.ts
+++ src/routes/condominiumType.ts
@@ -9,7 +9,7 @@ router.get('/search/:id', authenticate('jwt'), async (req: any, res) => {
   const { id } = req.params;
   const [err, data] = await fulfill(getCondominiumType(id));
   if (err) res.status(500).json({ err, status: 500 });
-  if (data) res.status(200).json(...data);
+  if (data) res.status(200).json(data);
 });
 
 router.put('/edit', authenticate('jwt'), async (req: any, res) => {});
