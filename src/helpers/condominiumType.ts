993764bde8a27bfeb41ec4102b2b18ac30430b9d
rodrigolamus <rodrigolamus@gmail.com>
2022-01-15 17:49:42 -0400

    fix in condominiumType helper

--- src/helpers/condominiumType.ts
+++ src/helpers/condominiumType.ts
@@ -10,9 +10,9 @@ export const getCondominiumType = async (id: number) => {
     const response = (await client.query(queries.GET_CONDOMINIUM_TYPE_BY_ID, [id])).rows[0];
     const response2 = (await client.query(queries.GET_ALL_CONDO_TYPES)).rows;
     const condoTypes = {
-      tipoA: response2.filter((elem) => elem.tipo_condominio === 'TIPO A').forEach((elem) => elem[0]),
-      tipoB: response2.filter((elem) => elem.tipo_condominio === 'TIPO B').forEach((elem) => elem[0]),
-      tipoC: response2.filter((elem) => elem.tipo_condominio === 'TIPO C').forEach((elem) => elem[0]),
+      tipoA: { ...response2.filter((elem) => elem.tipo_condominio === 'TIPO A') },
+      tipoB: { ...response2.filter((elem) => elem.tipo_condominio === 'TIPO B') },
+      tipoC: { ...response2.filter((elem) => elem.tipo_condominio === 'TIPO C') },
     };
     return { status: 200, message: 'tipo de condominio obtenido', tipoCondominio: response || 'no asignado', infoCondominios: condoTypes };
   } catch (e: any) {
