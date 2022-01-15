fa1482f5dd3289c7a720f15850efd842d57800eb
rodrigolamus <rodrigolamus@gmail.com>
2022-01-15 12:31:57 -0400

    fix in condominiumType helper

--- src/helpers/condominiumType.ts
+++ src/helpers/condominiumType.ts
@@ -7,7 +7,7 @@ const pool = Pool.getInstance();
 export const getCondominiumType = async (id: number) => {
   const client = await pool.connect();
   try {
-    const response = (await client.query(queries.GET_CONDOMINIUM_TYPE_BY_ID, [id])).rows;
+    const response = (await client.query(queries.GET_CONDOMINIUM_TYPE_BY_ID, [id])).rows[0];
     if (!response) throw new Error('Este condominio no tiene ningun tipo asignado');
     return { status: 200, message: 'tipo de condominio obtenido', tipoCondominio: response };
   } catch (e: any) {
