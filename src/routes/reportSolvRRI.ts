2746328a25924e806bff2b32d101e453f450f267
pablocastillo <pablocastillo325@gmail.com>
2022-02-09 09:26:20 -0400

    endpoint y pug reporte solv RRI

---
+++ src/routes/reportSolvRRI.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { fulfill } from '@utils/resolver';
+import { createMockCertificate } from '@utils/forms';
+
+const router = Router();
+
+router.get('/reportSolvRRI/:id', async (req: any, res) => {
+  const { id } = req.params;
+  const [error, data] = await fulfill(createMockCertificate(id));
+  if (error) res.status(500).json(error);
+  if (data)
+    data.toBuffer(async (err, buffer) => {
+      if (err) res.status(500).json({ status: 500, message: 'Error al procesar el pdf' });
+      res.contentType('application/pdf').send(buffer);
+    });
+});
+
+export default router;
