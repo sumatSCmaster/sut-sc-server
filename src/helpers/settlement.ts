c96640f9ddfbd12062bcb435f995a7f56bf10510
rodrigolamus <rodrigolamus@gmail.com>
2022-05-18 09:46:19 -0400

    console.logging

--- src/helpers/settlement.ts
+++ src/helpers/settlement.ts
@@ -3110,6 +3110,7 @@ export const addTaxApplicationPayment = async ({ payment, interest, application,
     const pagoSum = +payment.map((e) => fixatedAmount(+e.costo)).reduce((e, i) => e + i, 0);
     mainLogger.info('addTaxApplicationPayment -> pagoSum', pagoSum);
     mainLogger.info(`addTaxApplicationPayment -> ${payment.map((pay) => `concepto ${applicationType} referencia ${pay?.referencia} banco ${pay?.banco} metodo_pago ${pay.metodoPago}`).join(' , ')}`);
+    console.log(user);
     if (pagoSum < fixatedAmount(+solicitud.monto_total)) throw { status: 401, message: `La suma de los montos es insuficiente para poder insertar el pago, con un déficit de Bs. ${fixatedAmount(+solicitud.monto_total) - pagoSum}` };
     const creditoPositivo = pagoSum - fixatedAmount(+solicitud.monto_total);
     await Promise.all(
