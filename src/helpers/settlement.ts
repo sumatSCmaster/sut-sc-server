87fe171a8dcbfb7e96a1e61b3bc656bb98ee0924
rodrigolamus <rodrigolamus@gmail.com>
2022-06-02 16:25:09 -0400

    new build pack

--- src/helpers/settlement.ts
+++ src/helpers/settlement.ts
@@ -5250,6 +5250,7 @@ const createReceiptForPPApplication = async ({ gticPool, pool, user, application
   try {
     if (application.idSubramo !== 12) throw new Error('No se puede generar este recibo');
 
+    
     mainLogger.info('e');
     let certInfo;
     let certInfoArray: any[] = [];
