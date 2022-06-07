a301c569e7e44ab26ecbfd8f43a284f41dda5b03
rodrigolamus <rodrigolamus@gmail.com>
2022-06-07 11:41:56 -0400

    fixes

--- src/helpers/solvencies.ts
+++ src/helpers/solvencies.ts
@@ -6,8 +6,8 @@ const pool = Pool.getInstance();
 export const getSolvencyBCandidates = async ({tipoDocumento, documento}) => {
     const client = await pool.connect();
     try {
-        const solvencyRIMInfo = (await client.query(queries.GET_SOLVENCY_B_RIM_CANDIDATES_BY_RIF, [tipoDocumento, documento])).rows[0];
-        const solvencyContrInfo = await (await client.query(queries.GET_SOLVENCY_B_RIF_CANDIDATES_BY_RIF, [tipoDocumento, documento])).rows;
+        const solvencyRIMInfo = (await client.query(queries.GET_SOLVENCY_B_RIM_CANDIDATES_BY_RIF, [tipoDocumento, documento])).rows;
+        const solvencyContrInfo = await (await client.query(queries.GET_SOLVENCY_B_RIF_CANDIDATES_BY_RIF, [tipoDocumento, documento])).rows[0];
         const result = {contribuyente: solvencyContrInfo, sucursales: [...solvencyRIMInfo]}
         return {status: 200, data: result};
     } catch(e) {throw {status: 500, message: e.message}}
