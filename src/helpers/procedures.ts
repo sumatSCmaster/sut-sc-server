135704e0582ec67b10f2f7c1853ffe86243c105c
rodrigolamus <rodrigolamus@gmail.com>
2022-06-08 11:26:15 -0400

    console logging

--- src/helpers/procedures.ts
+++ src/helpers/procedures.ts
@@ -1551,6 +1551,7 @@ export const initProcedureAnalistAB = async (procedure, user: Usuario, client: P
   let costo, respState, dir, cert, datosP;
   try {
     datosP = { usuario: contribuyente };
+    console.log(datosP, user,id, 'LUIS CASTILLO');
     const response = (await client.query(queries.PROCEDURE_INIT, [procedure.tipo === 'b' ? 113 : 112, JSON.stringify(datosP), user.id])).rows[0];
     response.idTramite = response.id;
     const resources = (await client.query(queries.GET_RESOURCES_FOR_PROCEDURE, [response.idTramite])).rows[0];
@@ -1567,7 +1568,6 @@ export const initProcedureAnalistAB = async (procedure, user: Usuario, client: P
     //     })
     //   );
     // }
-    console.log(nextEvent, 'LUIS CASTILLO');
     pago.concepto = 'TRAMITE';
     pago.user = analyst;
     await insertPaymentCashier(pago, response.id, client);
