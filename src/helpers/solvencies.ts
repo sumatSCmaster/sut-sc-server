5bcc3969e6ee5e38a34c9e756bcb50cfb5f97385
rodrigolamus <rodrigolamus@gmail.com>
2022-06-09 10:54:38 -0400

    new fix endpoint sovency b

--- src/helpers/solvencies.ts
+++ src/helpers/solvencies.ts
@@ -27,7 +27,7 @@ export const getSolvencyACandidates = async ({tipoDocumento, documento}) => {
         const contribHasSolvencyB = (await client.query(queries.GET_SOLVENCY_A_RIM_CANDIDATES_BY_RIF, [tipoDocumento, documento, moment().subtract(3, 'month').format('YYYY-MM-DD')])).rows[0].exists;
         const solvencyContrInfo = (await client.query(`SELECT * FROM impuesto.contribuyente WHERE tipo_documento = $1 AND documento = $2`, [tipoDocumento, documento])).rows[0];
         solvencyContrInfo.hasSolvencyB = contribHasSolvencyB;
-        if (solvencyContrInfo) solvencyContrInfo.inmuebles = (await client.query('SELECT * FROM inmueble_urbano WHERE id_inmueble IN (SELECT id_inmueble FROM impuesto.inmueble_contribuyente WHERE id_contribuyente = $1)', [solvencyContrInfo.id_contribuyente])).rows;
+        solvencyContrInfo.inmuebles = (await client.query('SELECT * FROM inmueble_urbano WHERE id_inmueble IN (SELECT id_inmueble FROM impuesto.inmueble_contribuyente WHERE id_contribuyente = $1)', [solvencyContrInfo.id_contribuyente])).rows;
         //logica para las sucursales
         const solvencyRIMInfo = (await client.query('SELECT * FROM impuesto.registro_municipal WHERE id_contribuyente = (SELECT id_contribuyente FROM impuesto.contribuyente WHERE tipo_documento = $1 AND documento = $2)', [tipoDocumento, documento])).rows;
         const newSolvencyRIMInfo = (solvencyRIMInfo.length > 0) ? (await Promise.all(solvencyRIMInfo.map(async rim => {
@@ -35,7 +35,8 @@ export const getSolvencyACandidates = async ({tipoDocumento, documento}) => {
             if (rimHasSolvencyB) return {...rim, inmuebles: (await client.query('SELECT * FROM inmueble_urbano WHERE id_registro_municipal = $1', [rim.id_registro_municipal])).rows}
             return {...rim}
         }))) : [];
-        const result = {contribuyente: solvencyContrInfo || undefined, sucursales: [...newSolvencyRIMInfo]}
+        const newestSolvencyRIMInfo = newSolvencyRIMInfo.filter(rim => rim.inmuebles);
+        const result = {contribuyente: solvencyContrInfo || undefined, sucursales: [...newestSolvencyRIMInfo]}
         return {status: 200, data: result};
     } catch(e) {throw {status: 500, message: e.message}}
 }
