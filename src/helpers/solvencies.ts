55aa42d8a0302cd4a085d629e7ed35f2e6982850
rodrigolamus <rodrigolamus@gmail.com>
2022-06-09 09:48:19 -0400

    new fix endpoint a

--- src/helpers/solvencies.ts
+++ src/helpers/solvencies.ts
@@ -25,12 +25,12 @@ export const getSolvencyACandidates = async ({tipoDocumento, documento}) => {
         if (!contribHasUser) throw {status: 401, message: 'El contribuyente no posee un usuario asociado'};
         //Logica para el contribuyente
         const contribHasSolvencyB =  (await client.query(queries.GET_SOLVENCY_A_RIM_CANDIDATES_BY_RIF, [tipoDocumento, documento, moment().subtract(3, 'month').format('YYYY-MM-DD')])).rows[0];
-        const solvencyContrInfo = contribHasSolvencyB && (await client.query(queries.GET_SOLVENCY_B_RIF_CANDIDATES_BY_RIF, [tipoDocumento, documento])).rows[0];
+        const solvencyContrInfo = contribHasSolvencyB && (await client.query(`SELECT * FROM impuesto.contribuyente WHERE tipo_documento = $1 AND documento = $2`, [tipoDocumento, documento])).rows[0];
         if (solvencyContrInfo) solvencyContrInfo.inmuebles = (await client.query('SELECT * FROM inmueble_urbano WHERE id_inmueble IN (SELECT id_inmueble FROM impuesto.inmueble_contribuyente WHERE id_contribuyente = $1)', [solvencyContrInfo.id_contribuyente])).rows;
         //logica para las sucursales
         const solvencyRIMInfo = (await client.query('SELECT * FROM impuesto.registro_municipal WHERE id_contribuyente = (SELECT id_contribuyente FROM impuesto.contribuyente WHERE tipo_documento = $1 AND documento = $2)', [tipoDocumento, documento])).rows;
         const newSolvencyRIMInfo = (solvencyRIMInfo.length > 0) ? (await Promise.all(solvencyRIMInfo.map(async rim => {
-            const rimHasSolvencyB = await client.query(`SELECT EXISTS(SELECT COUNT(*) FROM tramite WHERE datos#>>'{usuario, rim}' = $1)`, [rim.referencia_municipal])
+            const rimHasSolvencyB = await client.query(`SELECT EXISTS(SELECT * FROM tramite WHERE datos#>>'{usuario, rim}' = $1)`, [rim.referencia_municipal])
             if (rimHasSolvencyB) return {...rim, inmuebles: (await client.query('SELECT * FROM inmueble_urbano WHERE id_registro_municipal = $1', [rim.id_registro_municipal])).rows}
             return {...rim}
         }))) : [];
