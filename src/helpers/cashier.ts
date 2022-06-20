2437c1c3e2d2848a7476759fc8fd152f050f2688
pablocastillo <pablocastillo325@gmail.com>
2022-06-20 16:14:56 -0400

    console logging

--- src/helpers/cashier.ts
+++ src/helpers/cashier.ts
@@ -69,6 +69,7 @@ export const generateCashierReport = async (user, payload: { day: Date }) => {
     return prev;
   }, {});
   try {
+    console.log(+cashierCash.reduce((a, c) => c.transacciones + a, 0), +cashierCash.reduce((a, c) => c.total + a, 0), cashierCash)
     return new Promise(async (res, rej) => {
       const html = renderFile(resolve(__dirname, `../views/planillas/hacienda-cierreCaja.pug`), {
         moment: require('moment'),
