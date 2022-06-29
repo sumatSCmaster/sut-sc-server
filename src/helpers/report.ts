b3e364b1923124efbdb6cf3031e67c9fb3d164ea
pablocastillo <pablocastillo325@gmail.com>
2022-06-28 22:45:27 -0400

    reporte

--- src/helpers/report.ts
+++ src/helpers/report.ts
@@ -6,8 +6,10 @@ import Pool from '@utils/Pool';
 import * as pdf from 'html-pdf';
 import moment from 'moment';
 import * as qr from 'qrcode';
+import S3Client from '@utils/s3';
 
 const pool = Pool.getInstance();
+const dev = process.env.NODE_ENV !== 'production';
 
 export const createRepotRMP = async () =>{
   const client = await pool.connect();
@@ -17,13 +19,32 @@ export const createRepotRMP = async () =>{
     let cash = (await client.query(queries.GET_ALL_CASH_TOTAL)).rows;
     let payDiffCash = (await client.query(queries.GET_ALL_PAY_DIFF_CASH_TOTAL)).rows;
 
-    const html = renderFile(resolve(__dirname, `../views/planillas/hacienda-RMP.pug`), {
-      cash,
-      transferDiffNow,
-      payDiffCash
-    });
+    return new Promise(async (res, rej) => {
+      const html = renderFile(resolve(__dirname, `../views/planillas/hacienda-RMP.pug`), {
+        cash,
+        transferDiffNow,
+        payDiffCash
+      });
 
-    return pdf.create(html, { format: 'Letter', border: '5mm', header: { height: '0px' }, base: 'file://' + resolve(__dirname, '../views/planillas/') + '/' })
+      pdf.create(html, { format: 'Letter', border: '5mm', header: { height: '0px' }, base: 'file://' + resolve(__dirname, '../views/planillas/') + '/' })
+        .toBuffer(async (err, buffer) => {
+          if (err) {
+            rej(err);
+          } else {
+            const bucketParams = {
+              Bucket: process.env.BUCKET_NAME as string,
+              Key: `/hacienda/ReporteRecaudadoIngresadoMetodoPago.pdf`,
+            };
+            await S3Client.putObject({
+              ...bucketParams,
+              Body: buffer,
+              ACL: 'public-read',
+              ContentType: 'application/pdf',
+            }).promise();
+            res(`${process.env.AWS_ACCESS_URL}/${bucketParams.Key}`);
+          }
+        })
+      })
     
   }  catch (error) {
       console.log('PABLO',error)
