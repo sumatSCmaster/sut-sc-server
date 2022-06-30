86636d440e2425f5e6e7fd771f2194b08326e927
pablocastillo <pablocastillo325@gmail.com>
2022-06-30 16:45:09 -0400

    reporte

--- src/routes/report.ts
+++ src/routes/report.ts
@@ -5,8 +5,9 @@ import { createRepotRMP, createReportRID } from '@helpers/report';
 
 const router = Router();
 
-router.get('/methodPay', authenticate('jwt'), async (req, res) => {
-  const [error, data] = await fulfill(createRepotRMP());
+router.post('/methodPay', authenticate('jwt'), async (req, res) => {
+  const {fecha} = req.body;
+  const [error, data] = await fulfill(createRepotRMP(fecha));
   if (error) res.status(500).json({ message: error.message, status: 500 });
   res.status(200).json({data, status: 200});
 });
