4e3a7823a8bcd0d1c7cd3607e120b229eec1d2c7
rodrigolamus <rodrigolamus@gmail.com>
2022-07-11 11:14:15 -0400

    fix vehiculos

--- src/helpers/settlement.ts
+++ src/helpers/settlement.ts
@@ -429,7 +429,7 @@ export const getSettlements = async ({ document, reference, type, user }: { docu
       // const SMDate = moment([lastVHPayment.year(), lastVHPayment.month(), 1]);
       // const dateInterpolationSM = Math.floor(now.diff(SMDate, 'M'));
       VH = vehicles.length > 0 ? vehicles.map(async vh => {
-        const lastVHSettlement = (await client.query(`SELECT * FROM impuesto.liquidacion WHERE id_subramo = (SELECT id_subramo FROM impuesto.subramo JOIN impuesto.ramo USING(id_ramo) WHERE descripcion_corta = $1) AND (datos#>>'{desglose, 0, vehiculo}')::INT = $2 ORDER BY datos#>>'{fecha, year}' DESC LIMIT 1`)).rows[0]?.datos?.fecha?.year;
+        const lastVHSettlement = (await client.query(`SELECT * FROM impuesto.liquidacion WHERE id_subramo = (SELECT id_subramo FROM impuesto.subramo JOIN impuesto.ramo USING(id_ramo) WHERE descripcion_corta = $1) AND (datos#>>'{desglose, 0, vehiculo}')::INT = $2 ORDER BY datos#>>'{fecha, year}' DESC LIMIT 1`, ['VH', vh.id_vehiculo])).rows[0]?.datos?.fecha?.year;
         const VHDate = moment([lastVHSettlement, 0, 1]);
         const interpolation = now.diff(VHDate, 'years');
         console.log(VHDate.format('YYYY-MM-DD'), now.format('YYYY-MM-DD'), interpolation);
