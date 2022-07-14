f4564f2d4ad78322846352c53239f7f70cf76c8f
pablocastillo <pablocastillo325@gmail.com>
2022-07-14 11:44:56 -0400

    rebaja

--- src/helpers/settlement.ts
+++ src/helpers/settlement.ts
@@ -2033,6 +2033,7 @@ export const getApplicationsAndSettlements = async ({ user }: { user: Usuario })
           const liquidaciones = (await client.query(queries.GET_SETTLEMENTS_BY_APPLICATION_INSTANCE, [el.id_solicitud])).rows;
           const docs = (await client.query(queries.GET_CONTRIBUTOR_BY_ID, [el.id_contribuyente])).rows[0];
           const state = (await client.query(queries.GET_APPLICATION_STATE, [el.id_solicitud])).rows[0].state;
+          const rim = client.query('SELECT * FROM impuesto.registro_municipal WHERE id_registro_municipal = $1', [liquidaciones[0]?.id_registro_municipal]).rows;
           const type = el.tipo_solicitud;
 
           return {
@@ -2045,6 +2046,7 @@ export const getApplicationsAndSettlements = async ({ user }: { user: Usuario })
             tipo: el.tipo_solicitud,
             rebajado: el.rebajado,
             estado: state,
+            isRebaja: rim.rebaja,
             referenciaMunicipal: liquidaciones[0]?.id_registro_municipal
               ? (await client.query('SELECT referencia_municipal FROM impuesto.registro_municipal WHERE id_registro_municipal = $1', [liquidaciones[0]?.id_registro_municipal])).rows[0]?.referencia_municipal
               : undefined,
