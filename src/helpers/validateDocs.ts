eb4e85dcb2a58e847cb819037eed2806d1cddced
rodrigolamus <rodrigolamus@gmail.com>
2022-07-20 04:07:53 -0400

    fixes

--- src/helpers/validateDocs.ts
+++ src/helpers/validateDocs.ts
@@ -8,20 +8,20 @@ export const validateDocById = async (id: string) => {
   const client = await pool.connect();
   const response = { message: '', status: 0, data: {} };
   try {
-    const res = await client.query(queries.GET_PROCEDURE_BY_ID, [id]);
-    const cert = await client.query(queries.GET_CERTIFICATE_BY_PROCEDURE_ID, [id]);
-    if (res.rowCount !== 0 && cert.rowCount !== 0) {
+    const res = await client.query(queries.GET_APPLICATION_BY_ID, [id]);
+    // const cert = await client.query(queries.GET_CERTIFICATE_BY_PROCEDURE_ID, [id]);
+    if (res.rowCount !== 0 && res.rows[0].aprobado) {
       const tramite = res.rows[0];
-      delete tramite.state;
-      const usuario = (await client.query(queries.GET_USER_INFO_BY_ID, [tramite.usuario])).rows[0];
-      const tipoTramite = (await client.query(queries.GET_ONE_PROCEDURE_INFO, [tramite.tipotramite])).rows[0];
-      const institucion = (await client.query(queries.GET_ONE_INSTITUTION_INFO, [tipoTramite.idInstitucion])).rows[0];
+      // delete tramite.state;
+      const usuario = (await client.query(queries.GET_USER_INFO_BY_ID, [tramite.id_usuario])).rows[0];
+      // const tipoTramite = (await client.query(queries.GET_ONE_PROCEDURE_INFO, [tramite.tipotramite])).rows[0];
+      // const institucion = (await client.query(queries.GET_ONE_INSTITUTION_INFO, [tipoTramite.idInstitucion])).rows[0];
       const data = {
         datosSolicitante: usuario,
-        tipoTramite,
-        institucion,
+        tipoTramite: 39,
+        institucion: 'HACIENDA',
         tramite,
-        urlCertificado: cert.rows[0].urlCertificado,
+        // urlCertificado: cert.rows[0].urlCertificado,
       };
       response.message = 'Tramite encontrado';
       response.status = 200;
