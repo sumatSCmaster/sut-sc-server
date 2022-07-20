d794ec005d338e1d00abbf71aecfdc622ab6982f
rodrigolamus <rodrigolamus@gmail.com>
2022-07-20 14:39:09 -0400

    fix vehicle

--- src/helpers/vehicles.ts
+++ src/helpers/vehicles.ts
@@ -297,7 +297,8 @@ export const linkVehicle = async(placa: string, id: number, isRim: boolean) => {
   const client = await pool.connect();
   try {
     await client.query('BEGIN');
-    const idVehiculo = (await client.query('SELECT id_vehiculo FROM impuesto.vehiculo WHERE placa_vehiculo = $1', [placa])).rows[0].id_vehiculo;
+    const idVehiculo = (await client.query('SELECT id_vehiculo FROM impuesto.vehiculo WHERE placa_vehiculo = $1', [placa])).rows[0]?.id_vehiculo;
+    if (!idVehiculo) throw {status: 401, message: 'El vehiculo no esta registrado en el sistema SUT'};
     const enlazadoARim = (await client.query('SELECT * FROM impuesto.vehiculo WHERE id_registro_municipal IS NOT NULL AND id_vehiculo = $1', [idVehiculo])).rows[0];
     const enlazadoARif = (await client.query('SELECT * FROM impuesto.vehiculo_contribuyente WHERE id_vehiculo = $1', [idVehiculo])).rows[0];
     if (enlazadoARif || enlazadoARim) throw {status: 406, message: 'El vehiculo ya esta enlazado a otro contribuyente'};
