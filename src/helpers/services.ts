680e0231ccbd68298fc51758de070060fbaf6eda
rodrigolamus <rodrigolamus@gmail.com>
2022-08-08 11:19:40 -0400

    fix tariff residential

--- src/helpers/services.ts
+++ src/helpers/services.ts
@@ -95,7 +95,7 @@ export const getCleaningTariffForEstate = async ({ estate, branchId, client }) =
       ? /*estate.metros_construccion && +estate.metros_construccion !== 0
         ? costoMts * PETRO * estate.metros_construccion
         :*/ +(await client.query(queries.GET_AE_CLEANING_TARIFF, [branchId])).rows[0].tarifa
-      : +(await client.query(queries.GET_RESIDENTIAL_CLEANING_TARIFF)).rows[0].monto * PETRO;
+      : +(await client.query(queries.GET_RESIDENTIAL_CLEANING_TARIFF)).rows[0].monto;
     // const tarifaAseo = calculoAseo / PETRO > limiteAseo ? PETRO * limiteAseo : calculoAseo;
     return +calculoAseo;
   } catch (error) {
