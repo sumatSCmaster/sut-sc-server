998f33d8622ff0f1aed508c8402dad99f85a1927
rodrigolamus <rodrigolamus@gmail.com>
2022-08-23 10:20:21 -0400

    adding relativo to RRI endpoint

--- src/routes/reportSolvRRI.ts
+++ src/routes/reportSolvRRI.ts
@@ -5,8 +5,8 @@ import { createRRICertificate } from '@utils/forms';
 const router = Router();
 
 router.get('/', async (req: any, res) => {
-  const { codigoCatastral, metrosTerreno, metrosConstruccion, clasificacion, avaluoTerreno, avaluoconstruccion, direccion, parroquia, tipoInmueble } = req.query;
-  const [error, data] = await fulfill(createRRICertificate(codigoCatastral, metrosTerreno, metrosConstruccion, clasificacion, avaluoTerreno, avaluoconstruccion, direccion, parroquia, tipoInmueble));
+  const { codigoCatastral, metrosTerreno, metrosConstruccion, clasificacion, avaluoTerreno, avaluoconstruccion, direccion, parroquia, tipoInmueble, relativo } = req.query;
+  const [error, data] = await fulfill(createRRICertificate(codigoCatastral, metrosTerreno, metrosConstruccion, clasificacion, avaluoTerreno, avaluoconstruccion, direccion, parroquia, tipoInmueble, relativo));
   if (error) res.status(500).json(error);
   if (data) res.status(200).json(data);
 });
