36497d4e468a0ebd0a5231a078373c483cd1194f
rodrigolamus <rodrigolamus@gmail.com>
2022-09-21 12:47:51 -0400

    console logging

--- src/utils/createRPR.ts
+++ src/utils/createRPR.ts
@@ -242,6 +242,7 @@ export const createIDR = async (payload: {from: Date, to: Date}) => {
   try{
     const {from, to} = payload;
     const data = (await client.query(queries.GET_IDR_DATA, [from, to])).rows;
+    console.log(data.reduce((a, c) => a + c.cantidadLiquidado, 0), data.reduce((a, c) => a + c.totalLiquidado, 0), data.reduce((a, c) => a + c.totalIngresado, 0), data.reduce((a, c) => a + c.cantidadIngresado, 0))
     return new Promise(async (res, rej) => {
       const html = renderFile(resolve(__dirname,  `../views/planillas/hacienda-IDR.pug`), {
         moment: require('moment'),
