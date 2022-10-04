121cc7052ec0e5d0e82114a7b00159521c19de6a
rodrigolamus <rodrigolamus@gmail.com>
2022-10-04 16:54:25 -0400

    elaborado por & hora

--- src/helpers/estate.ts
+++ src/helpers/estate.ts
@@ -542,7 +542,7 @@ export const generateCodCat = async (data, user) => {
   const client = await pool.connect();
   try {
     // const {codCat, datos, datosFisicos, linderos, oldCodCat, perimetro} = data;
-    const property = (await client.query('SELECT id_inmueble AS id, avaluo_construccion AS "totalConstruccion", avaluo_terreno AS "totalTerreno", metros_terreno AS "superficieTerreno", metros_construccion AS "superficieConstruccion" FROM inmueble_urbano JOIN impuesto.avaluo_inmueble USING (id_inmueble) WHERE id_inmueble = $1', [data.inmueble])).rows[0];
+    const property = (await client.query('SELECT id_inmueble AS id, metros_terreno AS "superficieTerreno", metros_construccion AS "superficieConstruccion" FROM inmueble_urbano JOIN impuesto.avaluo_inmueble USING (id_inmueble) WHERE id_inmueble = $1', [data.inmueble])).rows[0];
     const userName = (await client.query('SELECT nombre_completo FROM usuario WHERE id_usuario = $1', [user.id])).rows[0]?.nombre_completo
     const bucketKey = `//hacienda/CATASTRO/${property.id}/ceritifcado.pdf`;
     data.property = property;
